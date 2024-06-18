b18b2fc518f0e8718cf1dc54422f9635a96abd29
Saradha-Sivaraman <ssaradha85@gmail.com>
2024-06-18 14:43:15 +0530

    changes for fullname display

--- src/App.js
+++ src/App.js
@@ -1,74 +1,3 @@
-// // Form.js
-// import React, { useState } from 'react';
-
-
-// function Form() {
-// 	const [formData, setFormData] = useState({
-// 		firstname: '',
-// 		lastname: '',
-// 	});
-//     const [fullName,setFullName]=useState("");
-// 	const [isShow, setShow] = useState(false);
-	
-//   	const handleChange = (e) => {
-// 	const { name, value } = e.target;
-// 		setFormData({
-// 			...formData,
-// 			[name]: value,
-// 		});
-// 	};
-
-// 	const handleSubmit = (e) => {
-// 		e.preventDefault();
-// 		setShow(true);
-//   //setFullName("Full Name: "+formData.firstname+ " " +formData.lastname);
-//   setFullName(`Full Name: ${formData.firstname} ${formData.lastname}`);
-
-// 	};
-
-// 	return (
-// 		<div>
-// 			<h1>Full Name Display</h1>
-// 			<form onSubmit={handleSubmit}>
-// 				<div>
-// 					<label>
-// 						First Name:
-// 						</label>
-// 					<input
-//             type="text"
-// 						name="firstname"
-// 						value={formData.firstname}
-// 						onChange={handleChange}
-//            // pattern="[A-Za-z]{1,9}"  title="Only alphabets allowed"
-//            required
-//            	/>
-// 				</div>
-//         <div>
-// 					<label>
-// 						Last Name:
-// 						</label>
-// 					<input
-//          		type="text"
-// 						name="lastname"
-// 						value={formData.lastname}
-// 						onChange={handleChange}
-//              //pattern="[A-Za-z]{1,9}"  title="Only alphabets allowed"
-//              required
-// 					/>
-				
-// 				</div>
-// 		     <button 
-// 					type="submit">Submit</button>
-// 				 {isShow===true && <p>{fullName}</p>}
-// 			</form>
-		  
-//            </div>
-// 	);
-// }
-
-// export default Form;
-
-
 import React, { useState } from "react";
 
 const App = () => {
@@ -76,7 +5,7 @@ const App = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
 
-  const showText = (e) => {
+  const showFullName = (e) => {
     e.preventDefault();
     if (firstName !== "" && lastName !== "") {
       setShow(true);
@@ -84,7 +13,7 @@ const App = () => {
   };
 
   const handleChange = (e) => {
-    if (e.target.name == "firstName") {
+    if (e.target.name === "firstName") {
       setFirstName(e.target.value);
     } else {
       setLastName(e.target.value);
@@ -92,14 +21,14 @@ const App = () => {
   };
   return (
     <div>
-      <form onSubmit={showText}>
+      <form onSubmit={showFullName}>
         <h1>Full Name Display</h1>
         <label>First Name:</label>
         <input
           type="text"
           name="firstName"
           value={firstName}
-          onChange={handleChange}
+          onChange={handleChange} 
         />
         <br />
         <label>Last Name:</label>
